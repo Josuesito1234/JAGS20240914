8590504db3d166505202607041baa2b699fa6d9e
Josuesito1234 <sosajosue2006@outlook.com>
2024-09-16 09:58:23 -0600

    Agregar archivos de proyecto.

---
+++ JAGS20240914/Auth/IJwtAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿namespace JAGS20240914.Auth
+{
+    // Interfaz para un servicio de autentication JWT
+    public interface IJwtAuthenticationService
+    {
+        // Metodo para autenticar al usuario y generar un token JWT
+        string Authenticate(string userName);
+    }
+}
